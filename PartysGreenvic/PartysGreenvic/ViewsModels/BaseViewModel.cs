7c4855fce55099bac1bf6d27ca25978801ab019b
aceg852 <cgonzalez@greenvic.cl>
2018-06-28 11:49:50 -0400

    Refresh

---
+++ PartysGreenvic/PartysGreenvic/ViewsModels/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartysGreenvic.ViewsModels
+{
+    class BaseViewModel
+    {
+    }
+}
--- PartysGreenvic/PartysGreenvic/ViewsModels/ValidadorViewModel.cs
+++ PartysGreenvic/PartysGreenvic/ViewsModels/ValidadorViewModel.cs
@@ -7,25 +7,70 @@ namespace PartysGreenvic.ViewsModels
     using PartysGreenvic.Views;
     using System.Windows.Input;
     using Xamarin.Forms;
-
-    class ValidadorViewModel
+    using System.ComponentModel;
+    class ValidadorViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        #region Attributes
+        private string rut;
+        private string nombre;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
         #region Properties
         public string Rut
         {
-            get;
-            set;
+            get
+            {
+                return this.rut;
+            }
+            set
+            {
+                if (this.rut != value)
+                {
+                    this.rut = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Rut)));
+                }
+            }
         }
-        public Boolean IsRunning
+        public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                if (this.isEnabled != value)
+                {
+                    this.isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+                }
+            }
         }
         #endregion
         #region Constructors
         public ValidadorViewModel()
         {
-           // agregar funciones por defecto cuando inicie la aplicacion
+            // agregar funciones por defecto cuando inicie la aplicacion
+            this.isEnabled = true;
         }
         #endregion
         #region Commands
@@ -36,7 +81,6 @@ namespace PartysGreenvic.ViewsModels
                 return new RelayCommand(Check);
             }
         }
-
         private async void Check()
         {
             if (string.IsNullOrEmpty(Rut))
@@ -45,11 +89,18 @@ namespace PartysGreenvic.ViewsModels
                     "Error",
                     "Ingrese Rut",
                     "Aceptar");
+
                 return;
             }
-
+            this.isRunning = true;
+            this.isEnabled = false;
+            await Application.Current.MainPage.DisplayAlert(
+                "Invitación",
+                "Persona Autorizada",
+                "Aceptar");
+            this.isRunning = false;
+            this.isEnabled = true;
         }
-
         public ICommand NombreCommand
         {
             get
