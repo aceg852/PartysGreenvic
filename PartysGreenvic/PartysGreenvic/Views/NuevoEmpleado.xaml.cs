36224081834c2e1978ffb7a57d9f8ae8bada5d9b
aceg852 <cgonzalez@greenvic.cl>
2018-07-03 09:28:01 -0400

    Empleados

---
+++ PartysGreenvic/PartysGreenvic/Views/NuevoEmpleado.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PartysGreenvic.Views
+{
+    using Helpers;
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NuevoEmpleado : ContentPage
+	{
+		public NuevoEmpleado ()
+		{
+			InitializeComponent ();
+            btnAgregar.Clicked += BtnAgregar_Clicked;
+            using (var datos = new DataAccess())
+            {
+                ListDatos.ItemsSource = datos.GetEmpleado();
+            }
+		}
+
+        private async void BtnAgregar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtRut.Text))
+            {
+                await DisplayAlert("Error", "Datos Vacíos", "Aceptar");
+                return;
+            }
+        }
+    }
+}
